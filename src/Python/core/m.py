fcd4165626c6f36095c9c77ee4d1ee709c2ef953
miranayyk13729805132 <yyk15322009576@petalmail.com>
2024-02-17 21:48:09 +0800

    1.2.1

---
+++ src/Python/core/m.py
@@ -0,0 +1,19 @@
+print('欢迎来到 InsCode')
+class miranalist:
+    def __init__(self, a):
+        self.a = a
+    def ndim(self):
+        return 1
+        
+m1 = miranalist(123)
+m1.ndim()
+print(m1.ndim())
+
+# import os 
+print(40*"-")
+# 打印输入a，40的维度。
+a = 40;
+bb = miranalist(a);
+bb.ndim();
+print(bb.ndim());
+
--- src/Python/python_miranalist的雏形/miranalist.py
+++
@@ -1,9 +0,0 @@
-class miranalist:
-    def __init__(self, a):
-        self.a = a
-    def ndim(self):
-        print("1")
-        return
-        
-m1 = miranalist(123)
-m1.ndim()
