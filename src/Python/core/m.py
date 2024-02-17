8dce7f8baa8033aac302c07732534d832125c69f
miranayyk13729805132 <yyk15322009576@petalmail.com>
2024-02-17 22:17:58 +0800

    1.2.1

--- src/Python/core/m.py
+++ src/Python/core/m.py
@@ -1,19 +1,13 @@
-print('欢迎来到 InsCode')
+# 小numpy库
 class miranalist:
     def __init__(self, a):
         self.a = a
     def ndim(self):
         return 1
-        
-m1 = miranalist(123)
-m1.ndim()
-print(m1.ndim())
 
-# import os 
-print(40*"-")
-# 打印输入a，40的维度。
-a = 40;
-bb = miranalist(a);
-bb.ndim();
-print(bb.ndim());
+mx = input()
+# input a number.请输入一个数。
+
+m = miranalist(mx)
+print(m.ndim())
 
