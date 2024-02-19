7065ac0a4c7f375de82fa8c5eb78ff0274644ced
miranayyk13729805132 <yyk15322009576@petalmail.com>
2024-02-19 22:10:04 +0800

    1.3.2

--- src/Python/core/m.py
+++ src/Python/core/m.py
@@ -1,21 +1,24 @@
 # 小numpy库
-class miranalist:
-    def __init__(self, s):
-        # self.s = s
+class mirananumpy:
+    def __init__(self, ndim):
+        self.ndim = ndim
+    def __ndim__():
         pass
     def ndim(self):
-        return len(list(mirana_input_two))
-print("此程序目的是，你输入一个一维或者二维的数组，返回这个数组的维度。\n在Python中数组的格式为输入一个数组格式如[1,2,3,4]或者[[1,2,3,4][1,2,3]]")
+        cout = 0
+        for item in [1,2,3,4],[2,3,4,5,6]:
+            if isinstance(item, list):
+                count += 1
+        return count
 
-print("请输入一个数组: ")
 
-mirana_input_two = [1,2,3,4]
+print("mirananumpy")
+print("请用mirananumpy创建一个数组: ")
 
-print("您输入的数组为：", mirana_input_two)
-
-m2 = miranalist(mirana_input_two)
-
-print("您输入的这个数的维度是")
-
-print(m2.ndim())
-
+ 
+x = mirananumpy([[1,2,3,4],[2,3,4,5,6]])
+print(x)
+x1 = x.ndim
+print(x1)
+x2 = x.ndim()
+print(x2)
